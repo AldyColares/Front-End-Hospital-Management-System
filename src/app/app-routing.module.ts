4dbd4a81269b1d3b678c9e36943befc1dfa789ae
aldycolares <aldycolares@gmail.com>
2023-11-07 21:24:18 -0300

    bug  mar-form-firld.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { DoctorComponent } from './auth-form/auth/doctor/doctor.component';
 import { DoctorRegisterComponent } from './auth-form/register/doctor-register/doctor-register.component';
 
+
 const routes: Routes = [
-      {path: 'register-doctor', component: DoctorComponent},
-      {path: 'auth-doctor', component:DoctorRegisterComponent},
+  { path: '', redirectTo: '/auth-doctor', pathMatch: 'full' },
+  { path: 'register-doctor', component: DoctorComponent },
+  { path: 'auth-doctor', component: DoctorRegisterComponent },
+  // { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+   
+  ],
+  
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/auth-form/auth-form.module.ts
+++ src/app/auth-form/auth-form.module.ts
@@ -3,15 +3,21 @@ import { CommonModule } from '@angular/common';
 import { DoctorComponent } from './auth/doctor/doctor.component';
 import { DoctorRegisterComponent } from './register/doctor-register/doctor-register.component';
 
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     DoctorComponent,
     DoctorRegisterComponent
+   
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class AuthFormModule { }
